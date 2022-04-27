cd9812919dee01b737561b880a43d3bcd321ddd6
sakura-sakai <sakura.sakai.99@gmail.com>
2022-04-27 15:47:05 +0700

    Set orientation and status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app/app.dart';
@@ -8,6 +9,17 @@ import 'core/services/env/env.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// To set orientation always portrait
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  ///Set color status bar
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+    statusBarColor: Colors.transparent,
+  ));
 
   await EnvManager.ensureInitialized();
 
